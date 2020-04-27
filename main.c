292afbeda65d4ee01edc7bfcfdce3517491385f5
pc <beata.jackowska99@gmail.com>
2020-04-27 17:33:05 +0200

    sth

---
+++ main.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<time.h>
+
+void bubble_sort(int n)
+{
+
+
+int array[100], c, d, swap ;
+
+
+
+
+srand(time(NULL));
+	for(int j=0; j<n;j++)
+	{
+
+		array[j] = rand();
+		printf("%d ",array[j]);
+	}
+
+  for (c = 0 ; c < n - 1; c++)
+  {
+    for (d = 0 ; d < n - c - 1; d++)
+    {
+      if (array[d] > array[d+1]) /* For decreasing order use < */
+      {
+        swap       = array[d];
+        array[d]   = array[d+1];
+        array[d+1] = swap;
+      }
+    }
+  }
+  printf("\n");
+for(int i=0; i<n; i++) printf("%d ",array[i]);
+}
+
+
+
+
+
+
+int main()
+{
+
+    int size;
+
+	printf("Enter the number of numbers you want to sort: ");
+
+	scanf("%d", &size);
+
+
+	bubble_sort(size);
+
+
+
+    return 0;
+}
